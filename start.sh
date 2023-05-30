91f3a1cfa026de6a704f6b1583b5cb114ab021ec
Nathaniel Davidson <nathaniel.davidson@gmail.com>
2023-05-30 00:16:33 -0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 FILE=/ssh/key
-
-mkdir ~/.ssh/
-ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
-cp /ssh/key ~/.ssh/id_rsa
-chmod 0600 ~/.ssh/id_rsa
-
+if [ -f "$FILE" ]; then
+  mkdir ~/.ssh/
+  ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
+  cp /ssh/key ~/.ssh/id_rsa
+  chmod 0600 ~/.ssh/id_rsa
+fi
 
 if [[ -z "${GIT_BRANCH}" ]]; then
     git ls-remote $GIT_REPO $GIT_BRANCH |  awk '{ print $1}'
